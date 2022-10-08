8b31cb32de9c1ef1626de0c44bca89ec7f1ac94f
Alireza Oroumand <ar.oroumand@gmail.com>
2022-10-08 21:49:26 +0330

    03.PubSub

---
+++ 33. RabbitMqSamples/PubSub/RabbitSamples.PubSub.Sender/Program.cs
@@ -0,0 +1,24 @@
+﻿using RabbitMQ.Client;
+
+var connectionFactory = new ConnectionFactory()
+{
+    HostName = "localhost",
+};
+using var connection = connectionFactory.CreateConnection();
+using var channel = connection.CreateModel();
+
+string exchangeName = "MyMessageExchange";
+channel.ExchangeDeclare(exchangeName, ExchangeType.Fanout, false, false, null);
+
+
+Console.Write("Type your message and press [Enter]: ");
+string message = Console.ReadLine() ?? "Default message";
+
+var messageBytes = System.Text.Encoding.UTF8.GetBytes(message).ToArray();
+channel.BasicPublish(exchangeName,"", null, messageBytes);
+
+Console.WriteLine($"[*] your message sent: {message}");
+Console.WriteLine("Press [Enter] to exit.");
+Console.ReadLine();
+
+
