76a49594a0ac4c9e50f10ae4074364e9488c343a
Alireza Oroumand <ar.oroumand@gmail.com>
2022-10-08 15:50:47 +0330

    LoadBalancing

---
+++ 33. RabbitMqSamples/LoadBalancing/RabbitSamples.LoadBalancing.Receiver/Program.cs
@@ -0,0 +1,29 @@
+﻿using RabbitMQ.Client.Events;
+using RabbitMQ.Client;
+using System.Text;
+
+var connectionFactory = new ConnectionFactory()
+{
+    HostName = "localhost",
+};
+using var connection = connectionFactory.CreateConnection();
+using var channel = connection.CreateModel();
+string queueName = "LoadBalancing";
+channel.QueueDeclare(queueName, true, false, false, null);
+
+var cunsumer = new EventingBasicConsumer(channel);
+cunsumer.Received += Cunsumer_Received;
+
+channel.BasicConsume(queueName, false, cunsumer);
+Console.Write("Press [Enter] to exit.");
+Console.ReadLine();
+
+void Cunsumer_Received(object? sender, BasicDeliverEventArgs e)
+{
+    var body = e.Body.ToArray();
+    var stringMessage = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"[-] Start Processing message...: {stringMessage}");
+    Thread.Sleep(5000);
+    Console.WriteLine($"[-] Finish Processing message...: {stringMessage}");
+    channel.BasicAck(e.DeliveryTag, false);
+}
---
+++ 33. RabbitMqSamples/LoadBalancing/RabbitSamples.LoadBalancing.Sender/Program.cs
@@ -0,0 +1,25 @@
+﻿using RabbitMQ.Client;
+
+var connectionFactory = new ConnectionFactory()
+{
+    HostName = "localhost",
+};
+using var connection = connectionFactory.CreateConnection();
+using var channel = connection.CreateModel();
+string queueName = "LoadBalancing";
+channel.QueueDeclare(queueName, true, false, false, null);
+Console.Write("Type your message and press [Enter]: ");
+string message = Console.ReadLine() ?? "Default message";
+var messageProperty = channel.CreateBasicProperties();
+messageProperty.Persistent = true;
+for (int i = 0; i < 20; i++)
+{
+    var messageBytes = System.Text.Encoding.UTF8.GetBytes(message + i).ToArray();
+    channel.BasicPublish("", queueName, messageProperty, messageBytes);
+}
+
+Console.WriteLine($"[*] your message sent: {message}");
+Console.WriteLine("Press [Enter] to exit.");
+Console.ReadLine();
+
+
